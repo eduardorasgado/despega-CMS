e9e2732c8b9b79779fac6036f8adddfebfd19b20
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-05-01 01:28:13 -0500

    B:Se crea controlador templateFController y se anexa en index del front. F: Se crea templateFront en views de front y lo mismo para el back

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+/*==========================================
+=            INDEX.PHP FRONTEND            =
+==========================================*/
+require_once("controllers/templateFController.php");
+
+$templateController = new TemplateController();
+$templateController->template();
+
+
+/*=====  End of INDEX.PHP FRONTEND  ======*/
+
