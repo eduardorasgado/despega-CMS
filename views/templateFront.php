62ebd18a8e4593851a08e2b0b276f6b5846a9fa4
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-05-27 07:28:26 -0500

    F: Added footer and styling footer

--- views/templateFront.php
+++ views/templateFront.php
@@ -97,58 +97,66 @@
 
 			<hr>
 			
-			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+			<div class="container-fluid footer-1">
+				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
 			
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.0610775555!2d-75.60278588568637!3d6.255684295471969!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e4429739f2122e9%3A0x7097411dc6e57e48!2sCl.+45f+%2382-31%2C+Medell%C3%ADn%2C+Antioquia%2C+Colombia!5e0!3m2!1ses!2sus!4v1470838764806" width="100%"  frameborder="0" style="border:0" allowfullscreen></iframe>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d122042.35062006253!2d-96.80577198801356!3d17.081286080152847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85c72249df26d9b1%3A0xac88a77657dffc3b!2sOaxaca%2C+Oax.!5e0!3m2!1ses!2smx!4v1527422157473" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 jumbotron">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 jumbotron">
 
-            		<h4 class="blockquote-reverse text-primary">
-            			<ul>
-			              <li><span class="glyphicon glyphicon-phone"></span>  (57)(4) 234 56 43</li>
-			              <li><span class="glyphicon glyphicon-map-marker"></span>  Calle 45F 32 - 45</li>
-			              <li><span class="glyphicon glyphicon-envelope"></span>  logotipo@correo.com</li>    
-			          	</ul>      
-            		</h4>
-        
-       			</div>
+	            		<h4 class="blockquote-reverse text-primary">
+	            			<ul>
+				              <li><span class="glyphicon glyphicon-phone"></span>  987 123 1234</li>
+				              <li><span class="glyphicon glyphicon-map-marker"></span>  Calle 45F 32 - 45</li>
+				              <li><span class="glyphicon glyphicon-envelope"></span>  contacto@tuempresa.com</li>    
+				          	</ul>      
+	            		</h4>
+	        
+	       			</div>
 
+				</div>
+
+				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12" id="formulario" >
+
+					<ol>
+	            		<li>
+	                		<a href="http://www.facebook.com" target="_blank">
+	                  		<i class="fa fa-facebook" style="font-size:24px;"></i>  
+	               		 	</a>
+	           			</li>
+
+	            		<li>
+	                		<a href="http://www.youtube.com" target="_blank">  
+	                  		<i class="fa fa-youtube" style="font-size:24px;"></i>  
+	               			</a>
+	            		</li>
+	            
+	            		<li>
+	                		<a href="http://www.vimeo.com" target="_blank">
+	                  		<i class="fa fa-vimeo" style="font-size:24px;"></i>  
+	                		</a>
+	            		</li>
+	       			</ol>
+
+	       			<form>
+						    <input type="text" class="form-control"  placeholder="Nombre">
+		
+						    <input type="email" class="form-control" placeholder="Email">
+
+						    <textarea name="" id="" cols="30" rows="10" placeholder="Contenido del Mensaje" class="form-control"></textarea>
+
+						 
+						  	<input type="button" class="btn btn-default" value="Enviar">
+					</form>
+									
+
+				</div>
 			</div>
 
-			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12" id="formulario" >
-
-				<ol>
-            		<li>
-                		<a href="http://www.facebook.com" target="_blank">
-                  		<i class="fa fa-facebook" style="font-size:24px;"></i>  
-               		 	</a>
-           			</li>
-
-            		<li>
-                		<a href="http://www.youtube.com" target="_blank">  
-                  		<i class="fa fa-youtube" style="font-size:24px;"></i>  
-               			</a>
-            		</li>
-            
-            		<li>
-                		<a href="http://www.vimeo.com" target="_blank">
-                  		<i class="fa fa-vimeo" style="font-size:24px;"></i>  
-                		</a>
-            		</li>
-       			</ol>
-
-       			<form>
-					    <input type="text" class="form-control"  placeholder="Nombre">
-	
-					    <input type="email" class="form-control" placeholder="Email">
-
-					    <textarea name="" id="" cols="30" rows="10" placeholder="Contenido del Mensaje" class="form-control"></textarea>
-
-					 
-					  	<input type="button" class="btn btn-default" value="Enviar">
-				</form>
-								
-
+			<div class="container-fluid footer-2">
+				<div class="company-foot">
+					<p>Tu empresa &copy; 2018 - 2019. Todos los derechos reservados</p>
+				</div>
 			</div>
 
 		</footer>
