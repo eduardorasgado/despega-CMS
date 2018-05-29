1fec0b3511e6e8962f0dfc8bbeb36cbf6c89e557
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-05-28 20:16:46 -0500

    adding name, size and file validation to image article in backend

--- backend/controllers/gestorArticlesBController.php
+++ backend/controllers/gestorArticlesBController.php
@@ -2,8 +2,38 @@
 
 class gestorArticlesController
 {
+	//Mostrar imagenes en el article image section
 	public function uploadImageController($datos)
 	{
-		return true;
+		//arreglo de propiedades
+		list($ancho, $alto) = getimagesize($datos["imagenTemporal"]);
+
+		if ($ancho < 800 || $alto < 400)
+		{
+			return false;
+		}
+		else
+		{
+			//obtener la extension del archivo
+			$nameSplitted = explode(".",$datos["imagenTemporal"]);
+
+			$extension = end($nameSplitted);
+
+			//queda almacenado la extension de la imagen
+			$noLastElement = array_pop($nameSplitted);
+
+			//unir el array en string, del nombre pero ya 
+			//sin la externsion(que fue quitada por el pop)
+			$noExtension = implode(".", $nameSplitted);
+
+			//quitar espacios
+			$noSpaces = str_replace(" ", "_", $noExtension);
+
+			//elegir un numero aleatorio
+			$aleatorio = mt_rand(100,999);
+
+			//crear nombre complejo ṕara subirlo al server
+			$ruta = "../../views/images/articulos/article_".$noSpaces."_".$aleatorio;
+		}
 	}
 }
