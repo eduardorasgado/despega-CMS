7f2e0baae3b42d9ff8f51a7d5d19d331fc69812f
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-05-29 01:18:36 -0500

    DB conection and saving files and article content in article model request

--- backend/models/gestorArticlesBModel.php
+++ backend/models/gestorArticlesBModel.php
@@ -6,8 +6,20 @@ class gestorArticlesModel
 {
 	public function guardarArticuloModel($datos, $tabla)
 	{
-		$query = "";
+		$query = "INSERT INTO $tabla (titulo, introduccion, ruta, contenido) VALUES (:titulo, :introduccion, :ruta, :contenido)";
+
 		$stmt = ConexionModels::conexionModel()->prepare($query);
-		return true;
+
+		$stmt->bindParam(":titulo", $datos["titulo"], PDO::PARAM_STR);
+		$stmt->bindParam(":introduccion", $datos["introduccion"], PDO::PARAM_STR);
+		$stmt->bindParam(":ruta", $datos["ruta"], PDO::PARAM_STR);
+		$stmt->bindParam(":contenido", $datos["contenido"], PDO::PARAM_STR);
+		
+		if ($stmt->execute()) {
+			$stmt = null;
+			return true;
+		}
+		$stmt = null;
+		return false;
 	}
 }
